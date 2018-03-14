c7741e9134c473f80d2448922b98f12484364e72
Marcus Windecker <marcus@onxmaps.com>
2018-03-13 23:13:50 -0600

    Getting results back from the API using the traveler

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
+var Traveler = require('the-traveler').default
+var Enums = require('the-traveler/build/enums')
 var bodyParser = require('body-parser')
 var util = require('util')
-
 var express = require('express')
 var app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/stats', function(req, res) {
-	var reqText = req.body.text
-	
+var traveler = new Traveler({
+  apikey: '', // TODO: replace with .env var
+  userAgent: 'slack'
+});
+
+app.post('/api/raid', function(req, res) {
 	res.setHeader('Content-Type', 'application/json')
-  res.send(JSON.stringify({
-  	response_type: 'in_channel',
-  	text: util.format('Here\'s the raid report for %s on PSN: https://raid.report/ps/%s', reqText, reqText)
-  }))
+
+	var username = req.body.text
+
+	traveler.searchDestinyPlayer('2', username)
+    .then((player) => {
+    	if (player.Response.length === 0) {
+    		res.send(JSON.stringify('there was an error'))
+    	} else {
+	    	var membershipId = player.Response[0].membershipId
+	    	traveler.getProfile('2', membershipId, { components: 100 })
+	    		.then((profile) => {
+	    			var characterIds = profile.Response.profile.data.characterIds
+
+	    			res.send(JSON.stringify(characterIds))
+	    		})
+    	}
+    })
+
+ 
+	
+
+	// res.send(JSON.stringify({
+ //  	response_type: 'in_channel',
+ //  	text: util.format('Here\'s the raid report for %s on PSN: https://raid.report/ps/%s', reqText, reqText)
+ //  }))
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
